e6d6852f6fbb03117e288a3caf8618c66a3bf282
grigory <grigory@localhost.localdomain>
2023-01-29 20:31:08 +0300

    added scripts

--- config.h
+++ config.h
@@ -3,15 +3,18 @@
 #define CLICKABLE_BLOCKS
 #define LEADING_DELIMITER " "
 const Block blocks[] = {
-//	command // update interval/ update signal
-//	BLOCK("sb-mail",    1800, 17),
-//	BLOCK("sb-music",   0,    18),
-//	BLOCK("sb-disk",    1800, 19),
-//	BLOCK("sb-memory",  10,   20),
-//	BLOCK("sb-loadavg", 5,    21),
-//	BLOCK("sb-mic",     0,    26),
-//	BLOCK("sb-record",  0,    27),
-//	BLOCK("sb-volume",  0,    22),
-	BLOCK("sb-battery", 5,    23),
-	BLOCK("sb-date",    1,    24)
-};
+//	command / update interval / update signal
+//	block("sb-mail",        1800, 17),
+//	block("sb-music",       0,    18),
+//	block("sb-disk",        1800, 19),
+//	block("sb-memory",      10,   20),
+//	block("sb-loadavg",     5,    21),
+//	block("sb-mic",         0,    26),
+//	block("sb-record",      0,    27),
+//	BLOCK()
+	BLOCK("sb-volume",      0,    22),
+	BLOCK("sb-layout",      0,    20),
+	BLOCK("sb-brightness",  0,    23),
+	BLOCK("sb-battery",     5,    24),
+	BLOCK("sb-date",        1,    25)
+};                              
